6d47837741c814d0c7d95b4dce1a675cc05350c0
AliALsheikhAli <61668152+AliALsheikhAli@users.noreply.github.com>
2020-08-29 00:13:39 +0300

    Create Çiftlik sorusu.cs

---
+++ Çiftlik sorusu.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tavsan = 500;
+            double tavuk = 1750;
+            int yil = 0;
+            while (tavsan <= tavuk)
+            {
+                tavsan += tavsan * 17 / 100;
+                tavuk += tavuk * 92 / 1000;
+                yil += 1;
+
+            }
+            Console.WriteLine(yil);
+            Console.ReadLine();
+
+        }
+    }
+}
